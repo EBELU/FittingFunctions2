af1c1d2b2c736c28d10d565a8c7ba51fbc61a6a3
EBELU <er4416be-s@student.lu.se>
2023-12-03 03:16:12 +0100

    Added some packages

---
+++ FittingFunctions2.0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  3 03:11:19 2023
+
+@author: Erik Ewald
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class 
