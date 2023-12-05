707b0c5441997846be1bd77116ff7c08ef77241c
EBELU <er4416be-s@student.lu.se>
2023-12-05 19:30:28 +0100

    Added new automatic scatter correction

--- FittingFunctions2.1.py
+++ FittingFunctions2.1.py
@@ -14,6 +14,31 @@ from scipy.signal import peak_widths
 def GaussFunc(x, A, mu, sigma):
     return A*np.exp(-(x-mu)**2/(2*sigma**2))
 
+def width_custom(y_region, x_region, threshold = 200):
+        # Find the centre point value and index
+        mu = x_region[y_region == max(y_region)][0]
+        i_peak = y_region.argmax()
+        
+        # Scipy makes an approximation for the width
+        width = peak_widths(y_region, [i_peak], rel_height = 0.5)[0][0]
+        #Using the approximation two ragions are cut ot on each side of the peak
+        right_region = x_region > mu + width
+        left_region = x_region < mu - width
+        
+        # The gradient is calcualted
+        right_grad = np.gradient(y_region[right_region], 2)
+        left_grad = np.gradient(y_region[left_region], 2)
+        
+        # plt.plot(x_region[right_region], right_grad)
+        # plt.plot(x_region[left_region], left_grad)
+        
+        # The point closest to the peak in the gradient passing the threshold is
+        # returend as the width.
+        left_point = x_region[left_region][(left_grad <= threshold)][-1]
+        right_point = x_region[right_region][(right_grad >= -threshold)][0]
+        return(left_point, right_point)
+        
+        
 x = np.linspace(0, 100, 500)
 y = GaussFunc(x, 5, 20, 3)
 
@@ -22,28 +47,52 @@ class gaussian:
     def __init__(self, X, Y, region_start, region_stop, #Setting region with one peak of interest
                      mu_guess = None, A_guess = None, sigma_guess = None, #Manual set guesses
                      scatter_corr = True, scatter_corr_points = 3, # Scatter correction setting
-                     corr_left = None, corr_right = None):
+                     corr_left = None, corr_right = None, corr_thresh = 0.05):
         #Slice up region of interest
         region = (region_start < X) & (X < region_stop)
         x_region = X[region]
         y_region = Y[region]
         const_y_region = y_region
-                
+        
+        # Make  correction for background scatter
         if scatter_corr:
+            if not corr_left and not corr_left:
+                try:
+                    #Try to get boundries for correction, the funciton can be finiky
+                    corr_left, corr_right = width_custom(y_region, x_region, corr_thresh)
+                except:
+                    corr_left, corr_right = (0,0)
+                    print('\033[0;1;31m' + "Warning!" +'\033[0m')
+                    print(f"Automatic scatter correction failed on the region [{region_start}, {region_stop}]")
+                    print("Try selecting another region or changing the threshold")
+
             if not corr_left:
-                corr_left = region_start
+                    corr_left = region_start
             if not corr_right:
-                corr_right = region_stop
-            
+                    corr_right = region_stop
+                    
+            # The scatter_corr_points nr of data points outside the boundries for
+            # scatter correction and average them in both x- and y-axis.
             left_xselection = sum(X[X <= corr_left][-scatter_corr_points:])/scatter_corr_points
             right_xselection = sum(X[X >= corr_right][:scatter_corr_points])/scatter_corr_points
             left_yselection = sum(Y[X <= corr_left][-scatter_corr_points:])/scatter_corr_points
             right_yselection = sum(Y[X >= corr_right][:scatter_corr_points])/scatter_corr_points
             
+            # Make a linear fit to
             k, m = np.polyfit([left_xselection, right_xselection], [left_yselection, right_yselection], 1)
+            print([left_xselection, right_xselection])
+            print([left_yselection, right_yselection])
+            print(k, m)
+            # Make a linear function from the parameters
             corr_f = lambda x : k * x + m
+            # Subtract the background region from the y-region
             y_region = y_region - corr_f(x_region)
+            # Save correction data for plotting purposes
             self._corr_f = corr_f
+            self._corr_points = [corr_left, corr_right]
+        if scatter_corr == False:
+            self._corr_f = lambda x : x * 0
+            self._corr_points = [None, None]
         #Assign guesses
         if A_guess:
             A_g = A_guess
@@ -67,23 +116,28 @@ class gaussian:
             
         guesses = [A_g, mu_g, sigma_g]
         
-        plt.figure()
-        width = peak_widths(y_region, [y_region.argmax()], 0.95)[0][0]
-        test_1 = x_region > mu_g + width
-        test_2 = x_region < mu_g - width
-        plt.plot(x_region, ((y_region)))
-        plt.plot(x_region[y_region.argmax()], y_region[y_region.argmax()], ".")
-        plt.plot(x_region[test_1], y_region[test_1])
-        plt.plot(x_region[test_2], y_region[test_2])
-        
         """MÖG FRÅN DEBUGGING"""
+
+        # plt.figure()
+        # width = peak_widths(y_region, [y_region.argmax()], rel_height = 1)
+        # print(width[1])
+        # width = width[0][0]*2
+        # test_1 = x_region > mu_g + width
+        # test_2 = x_region < mu_g - width
+        # plt.plot(x_region, ((y_region)))
+        # plt.plot(x_region, (np.gradient(y_region, 2)))
+        # plt.hlines(0, 1050, 1250)
+        # plt.plot(x_region[y_region.argmax()], y_region[y_region.argmax()], ".")
+        # plt.plot(x_region[test_1], y_region[test_1])
+        # plt.plot(x_region[test_2], y_region[test_2])
+        
+
         # plt.plot(x_region, y_region)
         
         # print(y_region.argmax())
         # print(sigma_g)
         # print(y_region[8])
         # print(guesses)
-        
         #Fitting the parameters
         import warnings
         from scipy.optimize import OptimizeWarning
@@ -106,34 +160,42 @@ class gaussian:
                 self.covar_matrix = covar_matrix
                 self._region = [x_region, const_y_region]
                 self._region_limits = [region_start, region_stop]
-
+                self._areas = [sum(const_y_region),sum(y_region), sum(self._corr_f(x_region))]
             except (RuntimeError, OptimizeWarning, TypeError):
-                print("Gaussian fit failed! Try specifying another mu_guess.")
+                raise RuntimeError("Gaussian fit failed! Try specifying another region.")
 
+                
         
     def value(self, x):
         return GaussFunc(x, self.A, self.mu, self.sigma)
     def area(self):
         return self.A*np.abs(self.sigma)*np.sqrt(2*np.pi)
     
-    def plot(self):
+    def plot(self, xlabel = None, ylabel = None):
         plt.figure(figsize = [9,6])
         plt.plot(self._region[0], self._region[1], label = "Data", color = "deepskyblue")
-        plt.plot(self._region[0], self.value(self._region[0]), label = "Fitted Gaussian", color = "forestgreen")
+        plt.plot(self._region[0], self._corr_f(self._region[0]) + self.value(self._region[0]), label = "Fitted Gaussian", color = "forestgreen")
         try:
             plt.plot(self._region[0], self._corr_f(self._region[0]), label = "Scatter correction", color = "crimson")
         except(AttributeError):
             pass
+        plt.vlines(self._corr_points[0], 0, self.A * .8, color = "crimson")
+        plt.vlines(self._corr_points[1], 0, self.A * .8, color = "crimson")
+        plt.xlabel(xlabel,fontsize="large")
+        plt.ylabel(ylabel,fontsize="large")
         plt.legend(fontsize="large")
         plt.xticks(fontsize="large")
         plt.yticks(fontsize="large")
 
     def __str__(self):
         est_par = "Estimated paramters: A = {}, mu = {}, sigma = {}".format(
-            round(self.A, 4), round(self.mu, 4), round(self.sigma, 4))
+             round(self.A, 4), round(self.mu, 4), round(self.sigma, 4))
         uncert = "Uncertanties: \u03C3(A) = {}, \u03C3(mu) = {}, \u03C3(sigma) = {}".format(
             round(np.sqrt(self.covar_matrix[0][0]), 4), 
             round(np.sqrt(self.covar_matrix[1][1]), 4), 
             round(np.sqrt(self.covar_matrix[2][2]), 4))
+        add_info = "Addtional info: Max height = {}, G = {}, N = {}, B = {}".format(
+            round(self.A + self._corr_f(self.mu), 4), round(self._areas[0], 4),
+            round(self._areas[1], 4), round(self._areas[2], 4))
         covarmat = "Covariance matrix: \n {}".format(self.covar_matrix)
-        return est_par + "\n" + uncert +"\n\n" + covarmat + "\n\n"
+        return est_par + "\n" + uncert + "\n" + add_info + "\n\n" + covarmat + "\n\n"
