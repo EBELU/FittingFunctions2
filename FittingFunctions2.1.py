631dacb335805045bef42d40282b73492f233b56
EBELU <er4416be-s@student.lu.se>
2023-12-18 16:09:13 +0100

    Cleaned the code a bit

--- FittingFunctions2.1.py
+++ FittingFunctions2.1.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  3 12:11:19 2023
-
-@author: eewa
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -29,14 +21,11 @@ def width_custom(y_region, x_region, threshold = 200):
         right_grad = np.gradient(y_region[right_region], 2)
         left_grad = np.gradient(y_region[left_region], 2)
         
-        # plt.plot(x_region[right_region], right_grad)
-        # plt.plot(x_region[left_region], left_grad)
-        
         # The point closest to the peak in the gradient passing the threshold is
         # returend as the width.
         left_point = x_region[left_region][(left_grad <= threshold)][-1]
         right_point = x_region[right_region][(right_grad >= -threshold)][0]
-        return(left_point, right_point)
+        return (left_point, right_point)
         
 """Class to hold data for the gaussian"""
 class gaussian:
@@ -44,17 +33,17 @@ class gaussian:
     def __init__(self, X, Y, region_start, region_stop, #Setting region with one peak of interest
                      corr_left = None, corr_right = None, corr_thresh = 0.05, #Scatter correction region
                      mu_guess = None, A_guess = None, sigma_guess = None, #Manual set guesses
-                     scatter_corr = "auto", scatter_corr_points = 5):# Scatter correction setting
+                     scatter_corr = "auto", scatter_corr_points = 3):# Scatter correction setting
         if corr_left != None and corr_right != None:
             if corr_left < region_start or corr_right > region_stop: # Warning for incorrectly placed correction limits
                 print('\033[0;1;31m' + "Warning!" +'\033[0m')
-                print("Scatter point outside region!")
+                print("Scatter point outside region!\n")
             
         #Slice up region of interest
         region = (region_start < X) & (X < region_stop)
         x_region = X[region]
         y_region = Y[region]
-        const_y_region = y_region
+        const_y_region = y_region.copy()
         
         # Make  correction for background scatter
         if scatter_corr:
@@ -83,10 +72,6 @@ class gaussian:
             # Make a linear fit to
             k, m = np.polyfit([left_xselection, right_xselection], [left_yselection, right_yselection], 1)
             
-            # print([left_xselection, right_xselection])
-            # print([left_yselection, right_yselection])
-            # print(k, m)
-            
             # Make a linear function from the parameters
             corr_f = lambda x : k * x + m
             # Subtract the background region from the y-region
@@ -96,70 +81,41 @@ class gaussian:
             # Save correction data for plotting purposes
             self._corr_f = corr_f
             self._corr_points = [corr_left, corr_right]
-            self._areas = [sum(const_y_region[lin_region]),sum(y_region[lin_region]), sum(self._corr_f(x_region[lin_region]))]
+            self.G, self.N, self.B = [sum(const_y_region[lin_region]),sum(y_region[lin_region]), sum(self._corr_f(x_region[lin_region]))]
         if scatter_corr == False:
             self._corr_f = lambda x : x * 0
             self._corr_points = [None, None]
-            self._areas = [sum(y_region),sum(y_region), 0.]
-        #Assign guesses
-        if A_guess:
-            A_g = A_guess
-        else:
-            #The guess for A is automatically set to the largest y-value in the region.
-            A_g = max(y_region)
+            self.G, self.N, self.B  = [sum(y_region),sum(y_region), 0.]
             
-        if mu_guess:
-            mu_g = mu_guess
-        else:
+        #Assign guesses
+        if not A_guess:
+            # The guess for A is automatically set to the largest y-value in the region.
+            A_guess = max(y_region)
+            
+        if not mu_guess:
             # The guess for mu is automatically set to the value on the x-axis corresponsing
             # to the largest y-value in the region.
-            mu_g = x_region[y_region.argmax()]
+            mu_guess = x_region[y_region.argmax()]
 
-        if sigma_guess:
-            sigma_g = sigma_guess
-        else:
+        if not sigma_guess:
             # The guess for sigma is set automatically to 1/2 of the width of the peak
             # at half max.
-            sigma_g = peak_widths(y_region, [y_region.argmax()], 0.5)[0][0]
+            sigma_guess = peak_widths(y_region, [y_region.argmax()], 0.5)[0][0]
             
-        guesses = [A_g, mu_g, sigma_g]
-        
-        """MÖG FRÅN DEBUGGING"""
-
-        # plt.figure()
-        # width = peak_widths(y_region, [y_region.argmax()], rel_height = 1)
-        # print(width[1])
-        # width = width[0][0]*2
-        # test_1 = x_region > mu_g + width
-        # test_2 = x_region < mu_g - width
-        # plt.plot(x_region, ((y_region)))
-        # plt.plot(x_region, (np.gradient(y_region, 2)))
-        # plt.hlines(0, 1050, 1250)
-        # plt.plot(x_region[y_region.argmax()], y_region[y_region.argmax()], ".")
-        # plt.plot(x_region[test_1], y_region[test_1])
-        # plt.plot(x_region[test_2], y_region[test_2])
-        
-
-        # plt.plot(x_region, y_region)
-        
-        # print(y_region.argmax())
-        # print(sigma_g)
-        # print(y_region[8])
-        # print(guesses)
+        guesses = [A_guess, mu_guess, sigma_guess]
         
         # Handling the warning from SciPy as errors
         import warnings
         from scipy.optimize import OptimizeWarning
         with warnings.catch_warnings():
             warnings.simplefilter("error", OptimizeWarning)
-            ## perform the gaussian fit to the data:
+            # perform the gaussian fit to the data:
             try:
                 estimates, covar_matrix = curve_fit(GaussFunc,
                                         x_region,
                                         y_region,
                                         guesses)
-                
-               # print((estimates[0], estimates[1], abs(estimates[2])))
+
                 ## create a Gauss object with the fitted coefficients for better code readability
                 self.A = estimates[0]
                 self.mu = estimates[1]
@@ -176,10 +132,13 @@ class gaussian:
         return GaussFunc(x, self.A, self.mu, self.sigma)
     def area(self):
         return self.A*np.abs(self.sigma)*np.sqrt(2*np.pi)
-    def FWHM(self):
-        return 2.35 * np.array([self.sigma, np.sqrt(self.covar_matrix[2][2])])
-    def addinfo(self):
-        return [self.A + self._corr_f(self.mu), self._areas[0], self._areas[1],self._areas[2]]
+    def FWHM(self, uncertanty = False):
+        if uncertanty:
+            return 2.35 * np.array([self.sigma, np.sqrt(self.covar_matrix[2][2])])
+        else:
+            return 2.35 *self.sigma
+    def max_height(self):
+        return self.A + self._corr_f(self.mu)
     
     "Plotting"
     def plot(self, xlabel = None, ylabel = None):
@@ -208,8 +167,8 @@ class gaussian:
             round(np.sqrt(self.covar_matrix[1][1]), 4), 
             round(np.sqrt(self.covar_matrix[2][2]), 4))
         add_info = "Addtional info: Max height = {}, G = {}, N = {}, B = {}".format(
-            round(self.A + self._corr_f(self.mu), 4), round(self._areas[0]),
-            round(self._areas[1]), round(self._areas[2]))
+            round(self.A + self._corr_f(self.mu), 4), round(self.G),
+            round(self.N), round(self.B))
         covarmat = "Covariance matrix: \n {}".format(self.covar_matrix)
         return est_par + "\n" + uncert + "\n" + add_info + "\n\n" + covarmat + "\n\n"
 
@@ -217,12 +176,14 @@ class gaussian:
 def calibrate(Y, peak_regions, energies, plot = False, gauss = True):
     if len(peak_regions) != len(energies):
         raise IndexError("Nr of peaks must match number of energies provided!")
-    X = np.array(range(len(Y)))
+    X = np.arange(0, len(Y))
+    
     peaks = []
     gauss_peaks = []
+    
     for p_region in peak_regions:
         if gauss:
-            gauss_peak=gaussian(X,Y, region_start=p_region[0], region_stop=p_region[1], scatter_corr=True)
+            gauss_peak=gaussian(X,Y, p_region[0], p_region[1], scatter_corr=True)
             gauss_peaks.append(gauss_peak)
             peaks.append(gauss_peak.mu)
         else:
@@ -249,7 +210,7 @@ def calibrate(Y, peak_regions, energies, plot = False, gauss = True):
                 gauss_y = gauss_peaks[i].value(X[region]) + gauss_peaks[i]._corr_f(X[region]) 
                 plt.plot(gauss_x, gauss_peaks[i]._corr_f(X[region]), color = "red", label = "Scatter correction", linestyle=":")
                 plt.plot(gauss_x, gauss_y, color = "indigo", label = "Fitted gaussian")
-                plt.scatter(lin_f(gauss_peaks[i].mu), gauss_peaks[i].addinfo()[0], color = "teal", label = "Calibration peak")
+                plt.scatter(lin_f(gauss_peaks[i].mu), gauss_peaks[i].max_height(), color = "teal", label = "Calibration peak")
             else:
                 plt.scatter(lin_f(peaks[i]), y_val, color = "teal", label = "Calibration peak")
         plt.legend(fontsize="large")
@@ -260,6 +221,3 @@ def calibrate(Y, peak_regions, energies, plot = False, gauss = True):
         plt.legend(by_label.values(), by_label.keys(), fontsize="large")
         plt.show()
     return (calib_x, (k,m))
-
-
-
